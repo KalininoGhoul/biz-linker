cc10b0ed2a24f347097607d9da8ab8d679f0d995
Евгений Гааль <gaal.04@mail.ru>
2025-01-06 14:04:44 +0700

    product image not required

--- backend/app/helpers.php
+++ backend/app/helpers.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Storage;
 
 if (!function_exists('fileUrl')) {
-    function fileUrl(string $path): string
+    function fileUrl(?string $path): ?string
     {
+        if (is_null($path)) return null;
+
         return Storage::disk('public')->url($path);
     }
 }
