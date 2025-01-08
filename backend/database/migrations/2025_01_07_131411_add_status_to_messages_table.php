d7c041ba5b4f05c22ebe5f30ed5bc61f3f041b91
Евгений Гааль <gaal.04@mail.ru>
2025-01-08 13:57:39 +0700

    pin

--- backend/database/migrations/2025_01_07_131411_add_status_to_messages_table.php
+++ backend/database/migrations/2025_01_07_131411_add_status_to_messages_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('messages', function (Blueprint $table) {
-//            $table->dropColumn('status');
+            $table->dropColumn('status');
         });
     }
 };
