17f71b35ed46aed4c83e7962f675e2e11562cd5a
Евгений Гааль <gaal.04@mail.ru>
2025-05-06 23:17:31 +0700

    telescope

--- backend/bootstrap/providers.php
+++ backend/bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
     App\Services\Dadata\DadataServiceProvider::class,
     App\Services\Organization\OrganizationServiceProvider::class,
 ];
